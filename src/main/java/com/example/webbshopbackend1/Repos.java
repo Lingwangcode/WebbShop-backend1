7fcf4be10dc4775d6453da60ae4b4764423299c0
Ling Wang <amyling.wang@gmail.com>
2023-04-24 13:44:28 +0200

    Initial commit

---
+++ src/main/java/com/example/webbshopbackend1/Repos.java
@@ -0,0 +1,2 @@
+package com.example.webbshopbackend1;public class Repos {
+}
---
+++ src/main/java/com/example/webbshopbackend1/WebbShopBackend1Application.java
@@ -0,0 +1,13 @@
+package com.example.webbshopbackend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbShopBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbShopBackend1Application.class, args);
+	}
+
+}
