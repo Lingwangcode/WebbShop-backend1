dc697b0590f08198d9196f0d5fdb38861fee7062
Maria Hagelin <mthagelin@gmail.com>
2023-04-24 15:35:32 +0200

    added GET http://localhost:8080/items

--- src/main/java/com/example/webbshopbackend1/Controllers/ItemController.java
+++ src/main/java/com/example/webbshopbackend1/Controllers/ItemController.java
@@ -1,9 +1,22 @@
 package com.example.webbshopbackend1.Controllers;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import com.example.webbshopbackend1.Models.Item;
+import com.example.webbshopbackend1.Repos.ItemRepo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ItemController {
+    private final ItemRepo itemRepo;
+
+    public ItemController(ItemRepo itemRepo) {
+        this.itemRepo = itemRepo;
+    }
+
+    @RequestMapping("items")
+    public List<Item> getItems(){
+        return itemRepo.findAll();
+    }
 }
