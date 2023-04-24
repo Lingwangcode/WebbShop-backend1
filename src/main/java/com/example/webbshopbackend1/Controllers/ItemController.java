5aba94ff00520fdfd93fa2fc20c136ff7b40798c
Maria Hagelin <mthagelin@gmail.com>
2023-04-24 15:12:51 +0200

    Item MVC

---
+++ src/main/java/com/example/webbshopbackend1/Controllers/ItemController.java
@@ -0,0 +1,9 @@
+package com.example.webbshopbackend1.Controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+}
