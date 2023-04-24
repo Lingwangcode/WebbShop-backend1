5ee0aba937beec0a4ecb25e395b7b026c4dd2810
Ling Wang <amyling.wang@gmail.com>
2023-04-24 15:34:26 +0200

    fixed OrderRepo

--- src/main/java/com/example/webbshopbackend1/Repos/OrderRepo.java
+++ src/main/java/com/example/webbshopbackend1/Repos/OrderRepo.java
@@ -1,4 +1,7 @@
 package com.example.webbshopbackend1.Repos;
 
-public interface OrderRepo {
+import com.example.webbshopbackend1.Models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepo extends JpaRepository<Order, Long> {
 }
