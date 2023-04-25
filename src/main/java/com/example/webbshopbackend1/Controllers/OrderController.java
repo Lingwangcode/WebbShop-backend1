978a3a15cfa422c251112005f31b0bbdb0d68346
Ling Wang <amyling.wang@gmail.com>
2023-04-25 09:34:31 +0200

    .

--- src/main/java/com/example/webbshopbackend1/Controllers/OrderController.java
+++ src/main/java/com/example/webbshopbackend1/Controllers/OrderController.java
@@ -3,7 +3,6 @@ package com.example.webbshopbackend1.Controllers;
 import com.example.webbshopbackend1.Models.Orders;
 import com.example.webbshopbackend1.Repos.CustomerRepo;
 import com.example.webbshopbackend1.Repos.OrderRepo;
-import jakarta.persistence.criteria.Order;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
