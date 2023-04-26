90ce55c11c188dc44e12faebe462dea8fe41d820
Maria Hagelin <mthagelin@gmail.com>
2023-04-26 16:11:29 +0200

    commit before committing

--- src/test/java/com/example/webbshopbackend1/ItemControllerTest.java
+++ src/test/java/com/example/webbshopbackend1/ItemControllerTest.java
@@ -1,25 +1,55 @@
 package com.example.webbshopbackend1;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.reactive.world.model.UserDto;
+import com.reactive.world.serviceImpl.UserServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 import com.example.webbshopbackend1.Models.Item;
 import com.example.webbshopbackend1.Repos.ItemRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Arrays;
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -31,7 +61,7 @@ public class ItemControllerTest {
     private ItemRepo itemRepo;
 
     @BeforeEach
-    void beforeTest(){
+    void beforeTest() {
         Item item1 = new Item(1L, "Joggers", 599);
         Item item2 = new Item(2L, "Black sneakers", 859);
         Item item3 = new Item(3L, "Spongebob T-shirt", 150);
@@ -47,21 +77,52 @@ public class ItemControllerTest {
         this.mvc.perform(get("/items/getAll"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("[{\"id\":1,\"name\":\"Joggers\",\"price\":599}," +
-                        "{\"id\":2,\"name\":\"Blue sneakers\",\"price\":859}," +
-                        "{\"id\":3,\"name\":\"Spongebob t-shirt\",\"price\":150}]"));
+                        "{\"id\":2,\"name\":\"Black sneakers\",\"price\":859}," +
+                        "{\"id\":3,\"name\":\"Spongebob T-shirt\",\"price\":150}]"));
     }
 
     @Test
-    void getItemByIdTest(){
+    void getItemByIdTest() throws Exception {
+        this.mvc.perform(get("/items/getByItemId/1")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo("Joggers")))
+                .andExpect(jsonPath("$.id", equalTo(1)))
+                .andExpect(jsonPath("$.price", equalTo(599)));
 
+        this.mvc.perform(get("/items/getByItemId/2")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo("Black sneakers")))
+                .andExpect(jsonPath("$.id", equalTo(2)))
+                .andExpect(jsonPath("$.price", equalTo(859)));
+
+        this.mvc.perform(get("/items/getByItemId/3")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo("Spongebob T-shirt")))
+                .andExpect(jsonPath("$.id", equalTo(3)))
+                .andExpect(jsonPath("$.price", equalTo(150)));
     }
-/*
+
     @Test
-    void addItemTest(){
-        this.mvc.perform(get("/items/add"))
+    void addItemTest() throws Exception {
+
+        
+        this.mvc.perform(MockMvcRequestBuilders.post("/items/add"))
+                .andDo((ResultHandler) contentType(MediaType.APPLICATION_JSON))
+                .andDo((ResultHandler) content("{\"id\":5,\"name\":\"Hoodie\",\"price\":550}"))
+    //    Item item = new Item("Fine necklace", 9999);
+      //  Mockito.when(mvc.addItem(Mockito.any(Item.class))).thenReturn(item);
+
+    /*    mvc.perform(MockMvcRequestBuilders.post("/items/add")
+                        .content(asJsonString(item))
+                        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().json())
-    }*/
+                .andExpect(content().contentType("application/json;charset=UTF-8"));*/
+        /*
+        mvc.perform(MockMvcRequestBuilders
+                        .post("/items/add")
+                        .content(asJsonString(new Item(4L, "Necklace 4D", 260)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").exists()); //equalTo("Necklace 4D")-*/
+    }
 
     /*
 
@@ -80,4 +141,11 @@ public class ItemControllerTest {
         itemRepo.save(item);
         return itemRepo.findById(item.getId()).get();
     }*/
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
