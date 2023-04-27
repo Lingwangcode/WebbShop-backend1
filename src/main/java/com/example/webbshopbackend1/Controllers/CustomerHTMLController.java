8b3884c7cc127f7cf6942bd7c95f7a6a7c4e70e0
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2023-04-27 21:03:23 +0200

    rensat i filerna ändrat namn i url:ar

--- src/main/java/com/example/webbshopbackend1/Controllers/CustomerHTMLController.java
+++ src/main/java/com/example/webbshopbackend1/Controllers/CustomerHTMLController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -19,7 +18,7 @@ public class CustomerHTMLController {
     }
 
     @RequestMapping("/getAll")
-    public String getAllCustomers (Model model){
+    public String getAllCustomers(Model model){
         List<Customer> customers = customerRepo.findAll();
         model.addAttribute("allCustomers", customers);
         model.addAttribute("nameTitle", "Full name");
@@ -28,31 +27,8 @@ public class CustomerHTMLController {
         return "allCustomers.html";
     }
 
-    @RequestMapping("/getCards")
-    public String getAllCustomersWCards (Model model){
-        List<Customer> customers = customerRepo.findAll();
-        model.addAttribute("allCustomers", customers);
-        model.addAttribute("nameTitle", "Full name");
-        model.addAttribute("ssnTitle", "Social security number");
-        model.addAttribute("headline", "All customers");
-        return "allCustomersWCards.html";
-    }
-
-
-
     @RequestMapping("/getById/{id}")
-    public String getById (@PathVariable Long id, Model model){
-        Customer customer = customerRepo.findById(id).orElse(null);
-        model.addAttribute("fullname", customer.getName());
-        model.addAttribute("ssn", customer.getSocialSecurityNumber());
-        model.addAttribute("nameTitle", "Full name");
-        model.addAttribute("ssnTitle", "Social security number");
-        model.addAttribute("headline", "Specified customer");
-        return "oneCustomer.html";
-    }
-
-    @RequestMapping("/getByIdCard/{id}")
-    public String getByIdCard (@PathVariable Long id, Model model){
+    public String getById(@PathVariable Long id, Model model){
         Customer customer = customerRepo.findById(id).orElse(null);
         model.addAttribute("fullname", customer.getName());
         model.addAttribute("ssn", customer.getSocialSecurityNumber());
@@ -61,30 +37,14 @@ public class CustomerHTMLController {
         model.addAttribute("ssnTitle", "Social security number");
         model.addAttribute("idTitle", "Customer ID");
         model.addAttribute("headline", "All information on customer");
-        return "oneCustomerCard.html";
+        return "oneCustomer.html";
     }
 
-    @RequestMapping("/addByForm")
-    public String addByForm(Model model ){
-        return "addCustomerForm.html";
-    }
     @PostMapping("/save")
     public String save (@RequestParam String fullname,
                         @RequestParam String ssn, Model model){
         customerRepo.save(new Customer(fullname,ssn));
         return getAllCustomers(model);
-    }
-
-    @RequestMapping("/addByFormCard")
-    public String addByFormCard(Model model ){
-        model.addAttribute("headline", "Add customer");
-        return "addCustomerFormCard.html";
-    }
-    @PostMapping("/saveCard")
-    public String saveCard (@RequestParam String fullname,
-                        @RequestParam String ssn, Model model){
-        customerRepo.save(new Customer(fullname,ssn));
-        return getAllCustomersWCards(model);
     }
 
 }
