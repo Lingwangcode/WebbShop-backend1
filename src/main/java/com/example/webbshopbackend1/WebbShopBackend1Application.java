58e863fb4b5bb44862878134a787e2ef006871fb
Maria Hagelin <mthagelin@gmail.com>
2023-04-27 12:59:35 +0200

    removed comment

--- src/main/java/com/example/webbshopbackend1/WebbShopBackend1Application.java
+++ src/main/java/com/example/webbshopbackend1/WebbShopBackend1Application.java
@@ -21,7 +21,7 @@ public class WebbShopBackend1Application {
     public static void main(String[] args) {
         SpringApplication.run(WebbShopBackend1Application.class, args);
     }
-/*
+
     @Bean
     public CommandLineRunner bootstrap(ItemRepo itemRepo, CustomerRepo customerRepo, OrderRepo orderRepo) {
         return (args) -> {
@@ -61,5 +61,5 @@ public class WebbShopBackend1Application {
             orderRepo.save(order3);
         };
     }
- */
+
 }
