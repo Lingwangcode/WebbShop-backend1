24236f708331e3207f1ae6aa01c481f48d91d2e7
Ling Wang <amyling.wang@gmail.com>
2023-05-01 11:12:02 +0200

    Städat i OderController

--- src/main/java/com/example/webbshopbackend1/Controllers/OrderController.java
+++ src/main/java/com/example/webbshopbackend1/Controllers/OrderController.java
@@ -45,39 +45,6 @@ public class OrderController {
         return customerOrders;
     }
 
-  /*  @RequestMapping("/buy/{customerId}/{itemId}")
-    public String addOrder(@PathVariable Long customerId, @PathVariable Long itemId) {
-        Item item = itemRepo.findById(itemId).get();
-        Customer customer = customerRepo.findById(customerId).get();
-        if (item != null && customer != null) {
-            orderRepo.save(new Orders(LocalDate.now(), customer, List.of(item)));
-            return "Order added";
-        } else {
-            return "Order failed";
-        }
-    }
-
-   */
-
-    //curl -X POST -H "Content-Type: application/json" -d "{\"customerId\":\"1\",\"itemIds\":[\"2\",\"3\"]}" http://localhost:8080/orders/buy/1/2,3
-   /* @PostMapping(path = "/buy/{customerId}/{itemIds}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String addOrder(@PathVariable Long customerId, @PathVariable List<Long> itemIds) {
-        List<Item> items = new ArrayList<>();
-        for (Long itemId :itemIds) {            //måste gå via en for-loop för att kunna lägga till flera av samma id i samma order
-            Item item = itemRepo.findById(itemId).orElse(null);
-            if (item != null) {
-                items.add(item);
-            }
-        }
-        Customer customer = customerRepo.findById(customerId).orElse(null); //orElse(null) krävs för att inte få 500-fel om obefintligt ID anges
-        if (items != null && customer != null) {
-            orderRepo.save(new Orders(LocalDate.now(), customer, items));
-            return "Order added";
-        } else {
-            return "Order failed";
-        }
-
-    }*/
 
     @PostMapping(path = "/buy") //curl -X POST -H "Content-Type: application/json" "http://localhost:8080/orders/buy?customerId=1&itemIds=2&itemIds=3"
     public String addOrder(@RequestParam Long customerId, @RequestParam List<Long> itemIds) {
@@ -89,8 +56,8 @@ public class OrderController {
                 item.setStock(item.getStock()-1);
                 itemRepo.save(item);
             }
-            else {      //else sats för att breaka metoden att köra vidare
-                return "Order failed";
+            else {      //else sats för att breaka metoden att köra vidare vid fel itemid
+                return "Order failed, item id /ids not found";
             }
         }
         Customer customer = customerRepo.findById(customerId).orElse(null); //orElse(null) krävs för att inte få 500-fel om obefintligt ID anges
@@ -98,7 +65,7 @@ public class OrderController {
             orderRepo.save(new Orders(LocalDate.now(), customer, items));
             return "Order added";
         } else {
-            return "Order failed";
+            return "Order failed, customer id not found";
         }
 
     }
